f3a02243a9f98037550556b40c791c27a34dc424
danilocav <143456226+danilocav@users.noreply.github.com>
2023-08-28 17:53:59 -0400

    #0001_coding_rest_api
    Crud WEB.API rest

---
+++ MyWallWebAPI/MySQLContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWallWebAPI
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Post> Post { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>();
+        }
+
+    }
+}
